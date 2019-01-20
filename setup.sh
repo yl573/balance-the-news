ab50cc06221bc25917a014c0becb7b7b315e62e5
Li Xi <xl404@cam.ac.uk>
2019-01-20 07:43:25 +0000

    Updating script to run at home location

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+git clone https://github.com/yl573/balance-the-news.git
+cd balance-the-news
 sudo apt update
-sudo apt -y upgrade
 sudo apt install -y python3-pip
 pip3 install dill requests_html numpy newspaper3k
 python3 ./setup.py 
+# tmux new -s right python3 extract.py right
